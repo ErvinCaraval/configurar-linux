4c334186429611dd7dd3cb97ec5a2f5ce678a80b
ErvinCaraval <167554046+ErvinCaraval@users.noreply.github.com>
2025-09-05 16:42:16 -0500

    Update setup_zorin.sh
    cdddddddd

--- setup_zorin.sh
+++ setup_zorin.sh
@@ -269,6 +269,22 @@ fi
 
 echo "Versión de VirtualBox:" && virtualbox --version || true
 
+
+# -------------------------
+# Unity hub
+# -------------------------
+
+wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg > /dev/null
+
+
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+
+
+sudo apt update
+
+sudo apt-get install unityhub
+
+
 ## Finalization
 
 echo_header "Installation Process Completed"
